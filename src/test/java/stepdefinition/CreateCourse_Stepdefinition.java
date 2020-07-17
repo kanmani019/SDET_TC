969c449b4c720f07c0acbf77dfb023391c9e0307
kanmani019“git config --global user.name kanmani019“ <kanmam12@in.ibm.com>
2020-07-17 21:54:08 +0530

    commit

--- src/test/java/stepdefinition/CreateCourse_Stepdefinition.java
+++ src/test/java/stepdefinition/CreateCourse_Stepdefinition.java
@@ -3,13 +3,18 @@ package stepdefinition;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 //import org.openqa.selenium.support.ui.ExpectedConditions;
 //import org.openqa.selenium.support.ui.Select;
 //import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
 
+import common.Wrapper;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
@@ -28,8 +33,8 @@ public class CreateCourse_Stepdefinition {
 
 	@Then("^I enter my username and password$")
 	public void i_enter_my_username_and_password() throws Throwable {
-		driver.findElement(By.id("login")).sendKeys("admin");
-		driver.findElement(By.id("password")).sendKeys("admin@123");
+		driver.findElement(By.id("login")).sendKeys(Wrapper.getDataFromExcel(1,0));
+		driver.findElement(By.id("password")).sendKeys(Wrapper.getDataFromExcel(1,1));
 		driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
 	    
 	}
@@ -43,13 +48,13 @@ public class CreateCourse_Stepdefinition {
 
 	@Then("^Enter valid credential in Title textbox$")
 	public void enter_valid_credential_in_Title_textbox() throws Throwable {
-		driver.findElement(By.name("title")).sendKeys("testing");
+		driver.findElement(By.id("update_course_title")).sendKeys("testing");
 	   
 	}
 
 	@Then("^Enter valid credential in  code textbox$")
 	public void enter_valid_credential_in_code_textbox() throws Throwable {
-		driver.findElement(By.name("visual_code")).sendKeys("tes");
+		driver.findElement(By.id("visual_code")).sendKeys("tes");
 	    
 	}
 
@@ -59,8 +64,9 @@ public class CreateCourse_Stepdefinition {
 	public void select_Valid_credentials_from_Category_list_box() throws Throwable {
 		Actions act = new Actions(driver);
 		driver.findElement(By.xpath("//div[@class=\"filter-option-inner-inner\"]")).click();
-		driver.findElement(By.xpath("//input[@role=\"textbox\"]")).sendKeys("Projects");
-		act.click().perform();
+		WebElement Category = driver.findElement(By.xpath("//input[@aria-label=\"Search\"]"));
+		Category.sendKeys("Projects");
+		act.sendKeys(Keys.ENTER).build().perform();
 		
 		
 	    
@@ -69,20 +75,20 @@ public class CreateCourse_Stepdefinition {
 	@Then("^Select Valid credentials in Teacher list box$")
 	public void select_Valid_credentials_in_Teacher_list_box() throws Throwable {
 		Actions act = new Actions(driver);
-		driver.findElement(By.xpath("//input[@class=\"select2-search__field\"]")).sendKeys("manzoor");
-		Thread.sleep(7000);
-		act.click().perform();
-	
-		//Select sel = new Select (driver.findElement(By.xpath("//input[@class=\"select2-search__field\"]")));
-		//sel.selectByValue("1manzoor mehadi(2manzoor)");
-		
-	    
+		//driver.findElement(By.xpath("//input[@type=\"search\"]")).sendKeys("manzoor");
+		 //Select sel = new Select(driver.findElement(By.xpath("//input[@type=\"search\"]")));
+		 //Thread.sleep(6000);
+		 //sel.getFirstSelectedOption();
+		WebElement Teacher = driver.findElement(By.xpath("//input[@type=\"search\"]"));
+		Teacher.sendKeys("manzoor");
+		Thread.sleep(6000);
+		act.sendKeys(Keys.ENTER).build().perform();
+			    
 	}
 	
 
-	
 
-	@Then("^Click on Create a course button$")
+	@And("^Click on Create a course button$")
 	public void click_on_Create_a_course_button() throws Throwable {
 		driver.findElement(By.xpath("//button[@name=\"submit\"]")).click();
 	   
@@ -100,7 +106,7 @@ public class CreateCourse_Stepdefinition {
 
 	@Then("^Enter valid credentials in Name textbox$")
 	public void enter_valid_credentials_in_Name_textbox() throws Throwable {
-	    driver.findElement(By.name("name")).sendKeys("demo");
+		driver.findElement(By.id("usergroup_name")).sendKeys("demo");
 	}
 
 	@Then("^Enter valid credentials in Description textbox$")
@@ -112,12 +118,12 @@ public class CreateCourse_Stepdefinition {
 	public void select_valid_credentials_in_Group_permissions_list_box() throws Throwable {
 		Actions act = new Actions(driver);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//div[@class=\"filter-option-inner-inner\"]")).click();;
+		driver.findElement(By.xpath("//div[@class=\"filter-option-inner-inner\"]")).click();
 	    driver.findElement(By.xpath("//input[@role=\"textbox\"]")).sendKeys("open");
-	    act.click().perform();
+	    act.sendKeys(Keys.ENTER).build().perform();
 	}
 
-	@Then("^Click on Add button$")
+	@And("^Click on Add button$")
 	public void click_on_Add_button() throws Throwable {
 	    driver.findElement(By.name("submit")).click();
 	}
--- target/cucumber-reports/cucumber-pretty/report.js
+++ target/cucumber-reports/cucumber-pretty/report.js
@@ -70,56 +70,56 @@ formatter.match({
   "location": "CreateCourse_Stepdefinition.i_launch_the_application()"
 });
 formatter.result({
-  "duration": 10814327600,
+  "duration": 10469550700,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.i_enter_my_username_and_password()"
 });
 formatter.result({
-  "duration": 4240601200,
+  "duration": 5630307600,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.i_click_the_create_a_course_link()"
 });
 formatter.result({
-  "duration": 1779630800,
+  "duration": 1097230700,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.enter_valid_credential_in_Title_textbox()"
 });
 formatter.result({
-  "duration": 439406100,
+  "duration": 219046500,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.enter_valid_credential_in_code_textbox()"
 });
 formatter.result({
-  "duration": 295840700,
+  "duration": 121485200,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.select_Valid_credentials_from_Category_list_box()"
 });
 formatter.result({
-  "duration": 458523300,
+  "duration": 437532700,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.select_Valid_credentials_in_Teacher_list_box()"
 });
 formatter.result({
-  "duration": 7373630100,
+  "duration": 6261799600,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.click_on_Create_a_course_button()"
 });
 formatter.result({
-  "duration": 3048603200,
+  "duration": 1850866700,
   "status": "passed"
 });
 formatter.scenario({
@@ -180,56 +180,290 @@ formatter.match({
   "location": "CreateCourse_Stepdefinition.i_launch_the_application()"
 });
 formatter.result({
-  "duration": 9624718800,
+  "duration": 8209393600,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.i_enter_my_username_and_password()"
 });
 formatter.result({
-  "duration": 4437130000,
+  "duration": 3961759200,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.i_click_the_create_a_class_link()"
 });
 formatter.result({
-  "duration": 3096735300,
+  "duration": 2484689200,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.i_click_on_Add_classes_icon()"
 });
 formatter.result({
-  "duration": 779400500,
+  "duration": 811797200,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.enter_valid_credentials_in_Name_textbox()"
 });
 formatter.result({
-  "duration": 153312600,
+  "duration": 128895000,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.enter_valid_credentials_in_Description_textbox()"
 });
 formatter.result({
-  "duration": 266651700,
+  "duration": 135981800,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.select_valid_credentials_in_Group_permissions_list_box()"
 });
 formatter.result({
-  "duration": 390196300,
+  "duration": 363064900,
   "status": "passed"
 });
 formatter.match({
   "location": "CreateCourse_Stepdefinition.click_on_Add_button()"
 });
 formatter.result({
-  "duration": 1245491000,
+  "duration": 1052133200,
+  "status": "passed"
+});
+formatter.uri("CreateCourse_Excel.feature");
+formatter.feature({
+  "line": 2,
+  "name": "This feauture to automate admin to create course",
+  "description": "I want to use this template for my feature file",
+  "id": "this-feauture-to-automate-admin-to-create-course",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@CreateCourse"
+    }
+  ]
+});
+formatter.scenario({
+  "line": 6,
+  "name": "To verify whether application allows admin to create course",
+  "description": "",
+  "id": "this-feauture-to-automate-admin-to-create-course;to-verify-whether-application-allows-admin-to-create-course",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 5,
+      "name": "@CreateCourseCheck"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "I launch the application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I enter my username and password",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "I click the create a course link",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "Enter valid credential in Title textbox",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "Enter valid credential in  code textbox",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 12,
+  "name": "Select Valid credentials from Category list box",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "Select Valid credentials in Teacher list box",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 14,
+  "name": "Click on Create a course button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.i_launch_the_application()"
+});
+formatter.result({
+  "duration": 8298283200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.i_enter_my_username_and_password()"
+});
+formatter.result({
+  "duration": 4575246100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.i_click_the_create_a_course_link()"
+});
+formatter.result({
+  "duration": 1321142400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.enter_valid_credential_in_Title_textbox()"
+});
+formatter.result({
+  "duration": 268000200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.enter_valid_credential_in_code_textbox()"
+});
+formatter.result({
+  "duration": 197592700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.select_Valid_credentials_from_Category_list_box()"
+});
+formatter.result({
+  "duration": 615313900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.select_Valid_credentials_in_Teacher_list_box()"
+});
+formatter.result({
+  "duration": 6395075400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.click_on_Create_a_course_button()"
+});
+formatter.result({
+  "duration": 1995483700,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 18,
+  "name": "To verify whether application allows admin to create class",
+  "description": "",
+  "id": "this-feauture-to-automate-admin-to-create-course;to-verify-whether-application-allows-admin-to-create-class",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 17,
+      "name": "@CreateClassCheck"
+    }
+  ]
+});
+formatter.step({
+  "line": 19,
+  "name": "I launch the application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 20,
+  "name": "I enter my username and password",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 21,
+  "name": "I click the create a class link",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 22,
+  "name": "I click on Add classes icon",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 23,
+  "name": "Enter valid credentials in Name textbox",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 24,
+  "name": "Enter valid credentials in Description textbox",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 25,
+  "name": "Select valid credentials in Group permissions list box",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 26,
+  "name": "Click on Add button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.i_launch_the_application()"
+});
+formatter.result({
+  "duration": 9706791900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.i_enter_my_username_and_password()"
+});
+formatter.result({
+  "duration": 4817213100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.i_click_the_create_a_class_link()"
+});
+formatter.result({
+  "duration": 2525164400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.i_click_on_Add_classes_icon()"
+});
+formatter.result({
+  "duration": 887846400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.enter_valid_credentials_in_Name_textbox()"
+});
+formatter.result({
+  "duration": 166763000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.enter_valid_credentials_in_Description_textbox()"
+});
+formatter.result({
+  "duration": 285814400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.select_valid_credentials_in_Group_permissions_list_box()"
+});
+formatter.result({
+  "duration": 420327400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateCourse_Stepdefinition.click_on_Add_button()"
+});
+formatter.result({
+  "duration": 962027800,
   "status": "passed"
 });
 });
